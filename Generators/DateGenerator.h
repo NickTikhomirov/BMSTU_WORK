c2e20954df07e11ba23a96b3d1521a8d5c5066ef
NickTikhomirov <nicktikhomirov02@gmail.com>
2019-06-17 23:12:00 +0300

    other fixes

--- Generators/DateGenerator.h
+++ Generators/DateGenerator.h
@@ -13,30 +13,6 @@ public:
     vector<Date> dates;
     DateGenerator();
     void generate(char p=0);
-
-    /*
-    void generateYear(char);
-    void generateMonth();
-    void generateDay(char);
-    void initBad();
-public:
-
-    DateGenerator(short,short,short);
-    short d;
-    short m;
-    short y;
-
-    static DateGenerator d_today();
-    static string today();
-    static short getDays(short);
-    string toString();
-    void regenerate();
-
-    static string generateBad();
-    static string birthDate();
-
-    DateGenerator operator[](short);
-    */
 };
 
 #endif // DATEGENERATOR_H
